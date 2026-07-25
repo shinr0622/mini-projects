bceb1bdba79971ec4ea9474dfa39b9672dcdffc4
shin <shinr0622@gmail.com>
2026-07-25 22:15:45 +0800

    feat:物件、儲存練習

--- 260719_watame/script.js
+++ 260719_watame/script.js
@@ -20,16 +20,44 @@ function checkStock() {
   }
 }
 
+
 function calculateTotal() {
   let orderQty = Number(qtyInput.value);
-  let totalCost = orderQty * unitPrice;
-  priceDisplay.textContent = `總金額:$${totalCost}`;
+  let subTotal = orderQty * unitPrice;
+  let shippingFee = 60;
+  if (subTotal >= 2000) {
+    shippingFee = 0;
+    alert("恭喜免運！");
+  }
+
+  let discount = 0;
+  if (subTotal >= 1400) {
+    discount = 30;
+  }
+  let finaCost = subTotal + shippingFee - discount;
+  priceDisplay.textContent = `總金額:$${finaCost}(含運費:$${shippingFee},折抵:$${discount})`;
+
+  let orderBox = {
+    productName: "角卷綿芽布偶",
+    quantity: orderQty,
+    shippingCost: shippingFee,
+    discountAmount: discount,
+    totalTopay: finaCost,
+  };
+
+  let stringfiedData = JSON.stringify(orderBox);
+  localStorage.setItem("watameOrder", stringfiedData);
+  console.log(stringfiedData);
+
+
 }
 
 qtyInput.addEventListener("input", function () {
   checkStock();
   calculateTotal();
 });
+
+
 
 buyBtn.addEventListener("click", function () {
   let orderQty = Number(qtyInput.value);
@@ -39,8 +67,17 @@ buyBtn.addEventListener("click", function () {
     alert(`下單成功！您的網拍店目前剩餘庫存：${currentStock} 隻`);
 
     checkStock();
-
   } else {
     alert("下單失敗！您要購買的數量大於目前店內庫存。");
   }
 });
+
+
+  let savedData = localStorage.getItem("watameOrder");
+  if (savedData !== null){
+    let parsedBox = JSON.parse(savedData);
+
+    console.log("拆開後的包裹長這樣:",parsedBox );
+    console.log("上次客人的結帳總金額是:", parsedBox.totalTopay);
+    priceDisplay.textContent = `總金額:$${parsedBox.totalTopay} (這是上次離開前算好的喔！)`;
+  }
