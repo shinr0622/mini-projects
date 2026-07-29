873eec1239ec9e3eb7f4e068aa5f1949b913c409
shin <shinr0622@gmail.com>
2026-07-29 21:11:37 +0800

    feat:防呆檢查系統 findIndex

--- 260726_購物車/script.js
+++ 260726_購物車/script.js
@@ -24,7 +24,9 @@ const product = [
 console.log(product[0].price);
 
 const shopShelf = document.querySelector("#shop-shelf");
+
 product.forEach(function (item) {
+  //去product大棧板裡面，依序把每個箱子(物品) 放進商品貨架 最裡面
   shopShelf.innerHTML += `
     <div class="product-card">
       <img src="${item.img}" alt="${item.name}" class="product-img" >
@@ -32,7 +34,65 @@ product.forEach(function (item) {
       
       <p class="product-price">價格：$${item.price}</p>
       <p class="product-stock">剩餘庫存：${item.stock} 件</p>
-      <button class="add-cart-btn">加入購物車</button>
-    </div>
-`;
+      <button class="add-cart-btn" data-id="${item.id}">加入購物車</button>
+    </div>    
+`; //data-id="${item.id}"幫按鈕貼上隱形條碼貼紙
+});
+
+let cart = [];
+//推出一台空空的購物車
+const addCartBtns = document.querySelectorAll(".add-cart-btn");
+//定位 按鈕的名牌，一定要寫在生成卡片迴圈後面
+// 注意選擇器的All，是一次抓到全部大袋子按鈕 下面要寫一個個拿出來才行
+addCartBtns.forEach(function (btn) {
+  //派出迴圈機器人 對著任務指引 左手拿著按鈕大袋子位置 右手從每個袋子拿出按鈕(代名詞)
+  btn.addEventListener("click", function () {
+    //{幫每個按鈕綁定 點擊事件}
+
+    //注意:這裡是在檢查是否拿過相同的包裹id
+    let cartItemIndex = cart.findIndex(function (cartItem) {
+      //派出findIndex 機器人去 JS 的「推車陣列 (cart)」裡面巡視
+      return cartItem.id === Number(btn.dataset.id);
+      //機器人拿著 點擊的按鈕字串(轉換後)紙條，對比推車內是否有 相同的包裹id
+      //注意:cartItemIndex他回報的是車位號碼，不是id
+    });
+    //根據  findIndex 機器人回報 決定理貨方式:
+    if (cartItemIndex === -1) {
+      //狀況 A :機器人回報-1 (推車內沒有該商品)
+      let orderProduct = product.find(function (item) {
+        //派出find()機器人 去大棧板(product) 把客人要的包裹找出來
+        return item.id === Number(btn.dataset.id);
+        //嚴格比對 把HTML 貼紙字串(btn.dataset.id)，用number()轉換成純數字
+        //和大棧板上的純數字(id)做比對，注意 和貼到html{item.id}"是不同的
+        // 找到箱子的數字id 立刻停止比對
+
+        //注意如果改成name不需要number() 本身就是string文字字串
+      });
+
+      //orderProduct是「客人點擊的那個商品的原型參考」
+      //newCartItem 才是「客人真正放進推車裡的實體包裹」。抄寫完資訊後，那個原裝箱 (orderProduct) 就會原封不動地被放回大棧板上，沒有任何塗鴉，等待下一個客人下單時被拿出來參考。
+
+      //不直接使用orderProduct.quantity = 1; 理由是會造成Product陣列汙染，會多個數量1會出現無法預知的bug.
+      //所以下面額外抄寫個新紙箱
+      let newCartItem = {
+        //拿一個全新的空白物流紙箱 (創造一個新物件)
+        id: orderProduct.id,
+        name: orderProduct.name,
+        price: orderProduct.price,
+        img: orderProduct.img,
+        quantity: 1 //貼上這個客人專屬的「數量：1」貼紙！
+      };
+
+      cart.push(newCartItem);
+      console.log(`🛒 第一次購買！把【${newCartItem.name}】丟進車裡了！`);
+    } else {
+      // 狀況 B：機器人回報的不是 -1 (代表推車裡已經有這個商品了，它回報的是cart[車位號碼])
+      // 我們直接走到那個車位 (cart[cartItemIndex])，不需要推新的包裹進去，只要把原有的數量 +1
+      cart[cartItemIndex].quantity += 1;
+      console.log( `🔄 重複購買！【${cart[cartItemIndex].name}】的數量變成了 ${cart[cartItemIndex].quantity} 個！`,
+      );
+    }
+
+    console.log("目前的購物車內容:", cart); // 查看目前購物車陣列長怎樣
+  });
 });
